6f1bd046f2bbdd1771b537f9a714e1d4e9ba0605
antiqueproblemsolver <tarabrajpoot1@gmail.com>
2025-01-14 23:57:24 +0500

    1st day

---
+++ script.js
@@ -0,0 +1,61 @@
+//task 1
+// var a=1
+// var n=15
+// while (a<=n) {
+//     console.log(a);
+//     a++
+// }
+//task 2
+// var a=15
+// var n=1
+// while (a>=n) {
+// console.log(a);
+//     a--
+// }
+// task 3
+// var a=97
+// var n=122
+// while (a<=n) {
+// console.log(String.fromCodePoint(a));
+// a++    
+// }
+// task 4
+// var a=1
+// var n=100
+// while (a<n) {
+//     a++
+//     if (a%2==0) {
+//         console.log(a);
+        
+//     }
+// }
+// task 5
+// var a=1
+// var b=100
+// while (a<=b) {
+// if (a%2!==0) {
+//         console.log(a);
+        
+//     }
+//     a++
+// }
+//task 6
+//write a js program to print sum of all natural numbers from 1 to 100
+// var a=1 
+// var b=100
+// var sum=0
+// while (a<=b) {
+//     sum=sum+a
+//      a++
+//  }
+//  console.log(sum);
+
+var a=1
+var b=100
+var sum=0
+while (a<=b) {
+    if (a%2!==0) {
+        console.log(a++);
+        
+    }
+}
