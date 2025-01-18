8ef38c94f5e2d4913717b09e3e8993e00665c53e
antiqueproblemsolver <tarabrajpoot1@gmail.com>
2025-01-18 15:43:35 +0500

    completed

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 // var n=122
 // while (a<=n) {
 // console.log(String.fromCodePoint(a));
-// a++    
+// a++
 // }
 // task 4
 // var a=1
@@ -26,7 +26,7 @@
 //     a++
 //     if (a%2==0) {
 //         console.log(a);
-        
+
 //     }
 // }
 // task 5
@@ -35,13 +35,12 @@
 // while (a<=b) {
 // if (a%2!==0) {
 //         console.log(a);
-        
+
 //     }
 //     a++
 // }
 //task 6
-//write a js program to print sum of all natural numbers from 1 to 100
-// var a=1 
+// var a=1
 // var b=100
 // var sum=0
 // while (a<=b) {
@@ -49,13 +48,60 @@
 //      a++
 //  }
 //  console.log(sum);
-
-var a=1
-var b=100
-var sum=0
-while (a<=b) {
-    if (a%2!==0) {
-        console.log(a++);
-        
-    }
-}
+// task number 7
+// var a=1
+// var n=100
+// var sum=0
+// while (a<=n) {
+//     if (a%2==0) {
+//         sum=sum+a
+//     }
+//    a++
+// } console.log(sum);
+// task 8
+// var a=1
+// var n=100
+// var sum=0
+// while (a<=n) {
+//     if (a%2!=0) {
+//         sum=sum+a
+//     }
+//    a++
+// } console.log(sum);
+//task 9
+// var a=parseFloat(prompt('enter the number you want table of'))
+// var multipleOf=1
+// while (multipleOf<=10) {
+//     console.log(`${a}*${multipleOf}=${a*multipleOf}`);
+//     multipleOf++
+// }
+//  task 10
+// var a=String(prompt("enter the number"))
+// console.log('yau have '+a.length+' digits in your number');
+// task 11
+// var a = String(prompt("enter the number"))
+// console.log('The first digit in your number is ' + a.charAt(0) + '');
+// console.log('the last digit in your number is ' +  + '');
+// task 12
+// var a=String(prompt('enter the number'))
+// var firstDigit=parseInt(a.charAt(0))
+// var lastDigit=parseInt(a.charAt(a.length - 1))
+// console.log(`the sum of first and last digit of your number is ${firstDigit+lastDigit}`);
+//task 14
+// var a=String(prompt('Enter the numbers'))
+// var b=1
+// var sum=0
+// while (b<=a.length) {
+//     var sum=sum+b
+//     b++
+// }
+// console.log(sum);
+// task 15
+// var a=String(prompt('Enter the numbers'))
+// var b=1
+// var product=1
+// while (b<=a.length) {
+//     var product=product*b
+//     b++
+// }
+// console.log(product);
